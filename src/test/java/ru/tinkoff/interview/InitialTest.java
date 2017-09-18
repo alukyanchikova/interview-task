d245d0bfa28cab02b7078bfd1a575dfd56005d7e
alexandralukyanchikova <alexandralukyanchikova@gmail.com>
2017-09-18 23:25:28 +0300

    project skeleton

---
+++ src/test/java/ru/tinkoff/interview/InitialTest.java
@@ -0,0 +1,11 @@
+package ru.tinkoff.interview;
+
+import org.testng.annotations.Test;
+
+public class InitialTest {
+
+    @Test
+    public void should() {
+        System.out.println("test");
+    }
+}
